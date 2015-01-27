f4a92541154fd5093a64bc8a9ed4cfd3f49c5c47
Xiang Zhang <zhangxiangxiao@163.com>
2015-01-27 03:38:21 -0500

    rebuilding site Tue Jan 27 03:38:21 EST 2015

--- deploy.sh
+++ deploy.sh
@@ -16,5 +16,5 @@ fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push
+git push origin dev
 git subtree push --prefix=public git@github.com:zhangxiangxiao/zhangxiangxiao.github.io master
